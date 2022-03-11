89f84527159c925ffa09bfca0a5a2fad5f1b14a1
Madhu K <bhanumadhusagar@gmail.com>
2022-03-11 19:39:41 +0530

    added copy, paste, undo, redo, about functionalities

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "string"
+#include "QDebug"
+#include "QMessageBox"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -14,3 +17,64 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionOpen_2_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_As_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+        ui->textEdit_Notepad->copy();
+
+//        QMessageBox::warning(this, "Copy Action", "Please select text before copy");
+
+
+}
+
+
+void MainWindow::on_actionPase_triggered()
+{
+   if(!ui->textEdit_Notepad->canPaste())
+       QMessageBox::warning(this, "Paste Action", "Paste is empty");
+   else
+       ui->textEdit_Notepad->paste();
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit_Notepad->undo();
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit_Notepad->redo();
+}
+
+
+void MainWindow::on_actionAbout_Notepad_triggered()
+{
+    QMessageBox::information(this, "About", "A Simple notepad application as QT beginner");
+}
+
